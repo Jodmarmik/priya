c2ef4cc78ee535f4144b9b7b5cd8732c914544ec
DevilDeva08 <dalsingsaraibachpan@gmail.com>
2025-01-20 00:38:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Specify where to get the following credentials
-OWNER_USERNAME = getenv("OWNER_USERNAME", "II_CHALCOGEN_II")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_malware_ll")
 BOT_USERNAME = getenv("BOT_USERNAME", "Heer_chatbot")
 BOT_NAME = getenv("BOT_NAME", "˹𝐀ɴɴɪᴇ ✘ 𝙼ᴜsɪᴄ˼ ♪")
 ASSUSERNAME = getenv("ASSUSERNAME", "ll_love_xc_ll")
